564d940e6a2526f9c9d360ddeacfaf7a50266cb0
mxntxrx <hernandezmonteroadrian14@gmail.com>
2024-04-21 20:20:22 -0600

    Adding message input and sending it to server

--- src/socket-client.ts
+++ src/socket-client.ts
@@ -10,6 +10,9 @@ export const connectToServer = () => {
 const addListeners = (socket: Socket) => {
   const serverStatusLabel = document.querySelector("#server-status")!;
   const clientsUl = document.querySelector("#clients-ul")!;
+  const messageForm = document.querySelector<HTMLFormElement>("#message-form")!;
+  const messageInput =
+    document.querySelector<HTMLInputElement>("#message-input")!;
 
   socket.on("connect", () => {
     serverStatusLabel.innerHTML = "connected";
@@ -25,5 +28,17 @@ const addListeners = (socket: Socket) => {
       clientsHtml += `<li>${clientId}</li>`;
     });
     clientsUl.innerHTML = clientsHtml;
+  });
+
+  messageForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    if (messageInput.value.trim().length <= 0) return;
+
+    socket.emit("message-from-client", {
+      id: "I am ",
+      message: messageInput.value,
+    });
+
+    messageInput.value = "";
   });
 };
