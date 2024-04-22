85e1fba1b13acd643b013c94e706f9456b3e4116
mxntxrx <hernandezmonteroadrian14@gmail.com>
2024-04-21 19:39:28 -0600

    Printing client list on maints

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,8 @@ document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
   <div>
    <h1>Websocket - Client</h1>
    <span id="server-status">Connecting...</span>
+
+   <ul id="clients-ul"></ul>
   </div>
 `;
 
--- src/socket-client.ts
+++ src/socket-client.ts
@@ -9,6 +9,7 @@ export const connectToServer = () => {
 
 const addListeners = (socket: Socket) => {
   const serverStatusLabel = document.querySelector("#server-status")!;
+  const clientsUl = document.querySelector("#clients-ul")!;
 
   socket.on("connect", () => {
     serverStatusLabel.innerHTML = "connected";
@@ -16,5 +17,13 @@ const addListeners = (socket: Socket) => {
 
   socket.on("disconnect", () => {
     serverStatusLabel.innerHTML = "disconnected";
+  });
+
+  socket.on("clients-updated", (clients: string[]) => {
+    let clientsHtml = "";
+    clients.forEach((clientId) => {
+      clientsHtml += `<li>${clientId}</li>`;
+    });
+    clientsUl.innerHTML = clientsHtml;
   });
 };
