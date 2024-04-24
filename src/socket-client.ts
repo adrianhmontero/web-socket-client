541a0c88691c6013252391def5014da49a50ff3b
mxntxrx <hernandezmonteroadrian14@gmail.com>
2024-04-24 03:25:47 -0600

    Connecting to remote server

--- src/socket-client.ts
+++ src/socket-client.ts
@@ -3,7 +3,7 @@ import { Manager, Socket } from "socket.io-client";
 let socket: Socket;
 
 export const connectToServer = (token: string) => {
-  const manager = new Manager("http://localhost:3000/socket.io/socket.io.js", {
+  const manager = new Manager("https://seashell-app-sqpv3.ondigitalocean.app/socket.io/socket.io.js", {
     extraHeaders: {
       hola: "Mundo",
       authorization: token,
